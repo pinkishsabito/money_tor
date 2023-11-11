174affcadc156192bd324a5a00d1ecefdbf15e1e
pinkishsabito <ozhetov.arsen@gmail.com>
2023-11-11 22:30:10 +0600

    refactor: delete unused models

--- src/transaction/models.py
+++ src/transaction/models.py
@@ -12,7 +12,7 @@ class Currency(Base):
     name = mapped_column(VARCHAR(5), nullable=False)
 
 
-class Category():
+class Category(Base):
     __tablename__ = "categories"
 
     id = mapped_column(VARCHAR(36), primary_key=True, nullable=False)
@@ -20,19 +20,12 @@ class Category():
 
 
 class Transaction(Base):
-    __tablename__ = "users"
+    __tablename__ = "transactions"
 
     id = mapped_column(VARCHAR(36), primary_key=True, nullable=False)
     from_id = mapped_column(VARCHAR(36), ForeignKey("legal_entities.id"), nullable=False)
     to_id = mapped_column(VARCHAR(36), ForeignKey("legal_entities.id"), nullable=False)
     amount = mapped_column(INTEGER, nullable=False)
-    currency = mapped_column(VARCHAR(5), ForeignKey("currencies.name"))
+    currency = mapped_column(VARCHAR(5), ForeignKey("currencies.name"), nullable=False)
     created_at = mapped_column(TIMESTAMP, nullable=False)
     category = mapped_column(VARCHAR(36), ForeignKey("category.id"), nullable=False)
-
-
-class LegalEntity(Base):
-    __tablename__ = "legal_entities"
-
-    id = mapped_column(VARCHAR(36), nullable=False)
-    owner_id = mapped_column(ForeignKey("users.id"))
