c595a585bfaad2defd0a40776653d5354f79eafc
pinkishsabito <ozhetov.arsen@gmail.com>
2023-11-11 22:14:39 +0600

    feat: created models

--- src/transaction/models.py
+++ src/transaction/models.py
@@ -1,13 +1,38 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import VARCHAR, TIMESTAMP, ForeignKey, INTEGER
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import mapped_column
 
 Base = declarative_base()
 
 
-class User(Base):
+class Currency(Base):
+    __tablename__ = "currencies"
+
+    id = mapped_column(VARCHAR(36), primary_key=True, nullable=False)
+    name = mapped_column(VARCHAR(5), nullable=False)
+
+
+class Category():
+    __tablename__ = "categories"
+
+    id = mapped_column(VARCHAR(36), primary_key=True, nullable=False)
+    name = mapped_column(VARCHAR(200), nullable=False)
+
+
+class Transaction(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True)
-    email = Column(String(100), unique=True)
-    full_name = Column(String(100))
+    id = mapped_column(VARCHAR(36), primary_key=True, nullable=False)
+    from_id = mapped_column(VARCHAR(36), ForeignKey("legal_entities.id"), nullable=False)
+    to_id = mapped_column(VARCHAR(36), ForeignKey("legal_entities.id"), nullable=False)
+    amount = mapped_column(INTEGER, nullable=False)
+    currency = mapped_column(VARCHAR(5), ForeignKey("currencies.name"))
+    created_at = mapped_column(TIMESTAMP, nullable=False)
+    category = mapped_column(VARCHAR(36), ForeignKey("category.id"), nullable=False)
+
+
+class LegalEntity(Base):
+    __tablename__ = "legal_entities"
+
+    id = mapped_column(VARCHAR(36), nullable=False)
+    owner_id = mapped_column(ForeignKey("users.id"))
---
+++ src/user/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import VARCHAR, TIMESTAMP, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import mapped_column
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = mapped_column(VARCHAR(36), primary_key=True, nullable=False)
+    first_name = mapped_column(VARCHAR(30), nullable=False)
+    last_name = mapped_column(VARCHAR(30), nullable=False)
+    dob = mapped_column(TIMESTAMP, nullable=False)
+
+
+class LegalEntity(Base):
+    __tablename__ = "legal_entities"
+
+    id = mapped_column(VARCHAR(36), primary_key=True, nullable=False)
+    owner_id = mapped_column(VARCHAR(36), ForeignKey("users.id"), nullable=False)
